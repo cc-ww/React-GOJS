41ecf4ea1f5122843fdf2ada2a5f4fb56159db49
WangChen <853591143@qq.com>
2018-12-17 19:49:22 +0800

    for some css3 animation

--- my-app/src/testFour/test4.js
+++ my-app/src/testFour/test4.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './index.css';
+import { Button } from 'antd';
+import jquery from 'jquery';
+
+const $ = jquery;
 
 class TestFour extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			title:"TO like Css3",
+			myflag:0,
 		}
+		this.myAnimateClick = this.myAnimateClick.bind(this)
 	}
 
 	componentDidMount() {
+
+	}
+
+	myAnimateClick(){
+		this.state.myflag =  !this.state.myflag;
+		if(!this.state.myflag){
+			$('.ani_item').find("div").removeClass("ani_center")
+		}else{
+			$('.ani_item').find("div").addClass("ani_center")
+		}
+		
 	}
 
 	render() {
@@ -17,9 +34,12 @@ class TestFour extends React.Component {
 			<div className="contentBox">
 				<div className = "col-sm-12">
 					<h1>{this.state.title}</h1>
-					<div className = "myBox">
+					<Button type="primary" onClick={ this.myAnimateClick }>动画TOGGLE开关</Button>
+					<div className = "myBox2">
 						<div className = "ani_item">
 							<h1>Sugar!Sugar!</h1>
+							<div className = "ani_center ani_center_top"></div>
+							<div className = "ani_center ani_center_bottom"></div>
 						</div>
 					</div>
 				</div>
